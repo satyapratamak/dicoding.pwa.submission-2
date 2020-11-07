7a52157f99a1d43127a2bebda5ba64db46bd4e0f
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2020-11-08 01:05:00 +0700

    COMMIT : Success to create card for 5 top european footbal league

--- js/api.js
+++ js/api.js
@@ -1,4 +1,10 @@
 const urlBundesligaStandings = "https://api.football-data.org/v2/competitions/2002/standings";
+const urlEredivisieStandings = "https://api.football-data.org/v2/competitions/2003/standings";
+const urlEPLStandings = "https://api.football-data.org/v2/competitions/2021/standings";
+const urlLaLigaStandings = "https://api.football-data.org/v2/competitions/2014/standings";
+const urlLigue1Standings = "https://api.football-data.org/v2/competitions/2015/standings";
+
+
 
 // Blok kode yang akan di panggil jika fetch berhasil
 const status = (response) => {
@@ -27,6 +33,7 @@ const error = (error) => {
 
 // Blok kode untuk melakukan request data json
 const getBundesligaStandings = () => {
+  
     fetch(urlBundesligaStandings, {
         headers: {
             'X-Auth-Token' : '0e42cd9c1953405f81134678fb75b5dd'
@@ -34,29 +41,222 @@ const getBundesligaStandings = () => {
     })
       .then(status)
       .then(json)
-      .then(function(data) {
-        // Objek/array JavaScript dari response.json() masuk lewat data.
-        // Menyusun komponen card artikel secara dinamis
-        /*var articlesHTML = "";
-        data.result.forEach(function(article) {
-          articlesHTML += `
-                <div class="card">
-                  <a href="./article.html?id=${article.id}">
-                    <div class="card-image waves-effect waves-block waves-light">
-                      <img src="${article.thumbnail}" />
-                    </div>
-                  </a>
-                  <div class="card-content">
-                    <span class="card-title truncate">${article.title}</span>
-                    <p>${article.description}</p>
-                  </div>
-                </div>
-              `;
-        });
-        // Sisipkan komponen card ke dalam elemen dengan id #content
-        document.getElementById("articles").innerHTML = articlesHTML;*/
-        
-        console.log(data["standings"][0]);
+      .then( (data) => {
+        var articlesHTML = ``;
+        const obj1 = data["standings"][0];
+        let i = 0;
+        for (let iter_1 in obj1){
+          if (i == 3){            
+            const name = obj1[iter_1][0]["team"]["name"];
+            const points = obj1[iter_1][0]["points"];
+            const playedGames = obj1[iter_1][0]["playedGames"];
+            const won = obj1[iter_1][0]["won"];
+            const draw = obj1[iter_1][0]["draw"];
+            const lost = obj1[iter_1][0]["lost"];
+            const card = document.createElement("div");
+            card.className = "card blue-grey darken-1";
+            card.innerHTML = `<div class="card-content white-text">
+                                  <span class="card-title">${name} - ${points} Points</span>
+                                  <p>Played = ${playedGames}</p>
+                                  <p>Win = ${won}</p>
+                                  <p>Draw = ${draw}</p>
+                                  <p>Lost = ${lost}</p>
+                              </div>
+                              <div class="card-action">
+                                <a href="#">Full Bundesliga Table</a>                        
+                              </div> `;              
+              document.getElementById("articles").appendChild(card);                          
+          }
+          i++;          
+        }        
       })
       .catch(error);
   }
+
+// Blok kode untuk melakukan request data json
+const getEredivisieStandings = () => {
+  fetch(urlEredivisieStandings, {
+      headers: {
+          'X-Auth-Token' : '0e42cd9c1953405f81134678fb75b5dd'
+      }
+  })
+    .then(status)
+    .then(json)
+    .then( (data) => {
+      // Objek/array JavaScript dari response.json() masuk lewat data.
+      // Menyusun komponen card artikel secara dinamis
+      const obj1 = data["standings"][0];
+      let i = 0;
+      for (let iter_1 in obj1){
+        if (i == 3){            
+          const name = obj1[iter_1][0]["team"]["name"];
+          const points = obj1[iter_1][0]["points"];
+          const playedGames = obj1[iter_1][0]["playedGames"];
+          const won = obj1[iter_1][0]["won"];
+          const draw = obj1[iter_1][0]["draw"];
+          const lost = obj1[iter_1][0]["lost"];
+        
+          const card = document.createElement("div");
+          card.className = "card blue-grey darken-1";
+          card.innerHTML = `<div class="card-content white-text">
+                              <span class="card-title">${name} - ${points} Points</span>
+                              <p>Played = ${playedGames}</p>
+                              <p>Win = ${won}</p>
+                              <p>Draw = ${draw}</p>
+                              <p>Lost = ${lost}</p>
+                            </div>
+                            <div class="card-action">
+                              <a href="#">Full Eredivisie Table</a>                        
+                            </div>`;
+            
+          document.getElementById("articles").appendChild(card); 
+        }
+        i++;
+      }               
+    })
+    .catch(error);
+}
+
+// Blok kode untuk melakukan request data json
+const getEPLStandings = () => {
+  fetch(urlEPLStandings, {
+      headers: {
+          'X-Auth-Token' : '0e42cd9c1953405f81134678fb75b5dd'
+      }
+  })
+    .then(status)
+    .then(json)
+    .then( (data) => {
+      // Objek/array JavaScript dari response.json() masuk lewat data.
+      // Menyusun komponen card artikel secara dinamis
+      const obj1 = data["standings"][0];
+      let i = 0;
+      for (let iter_1 in obj1){
+        if (i == 3){            
+          const name = obj1[iter_1][0]["team"]["name"];
+          const points = obj1[iter_1][0]["points"];
+          const playedGames = obj1[iter_1][0]["playedGames"];
+          const won = obj1[iter_1][0]["won"];
+          const draw = obj1[iter_1][0]["draw"];
+          const lost = obj1[iter_1][0]["lost"];
+        
+          const card = document.createElement("div");
+          card.className = "card blue-grey darken-1";
+          card.innerHTML = `<div class="card-content white-text">
+                              <span class="card-title">${name} - ${points} Points</span>
+                              <p>Played = ${playedGames}</p>
+                              <p>Win = ${won}</p>
+                              <p>Draw = ${draw}</p>
+                              <p>Lost = ${lost}</p>
+                            </div>
+                            <div class="card-action">
+                              <a href="#">Full EPL Table</a>                        
+                            </div>`;
+            
+          document.getElementById("articles").appendChild(card); 
+        }
+        i++;
+      }               
+    })
+    .catch(error);
+}
+
+// Blok kode untuk melakukan request data json
+const getLaLigaStandings = () => {
+  fetch(urlLaLigaStandings, {
+      headers: {
+          'X-Auth-Token' : '0e42cd9c1953405f81134678fb75b5dd'
+      }
+  })
+    .then(status)
+    .then(json)
+    .then( (data) => {
+      // Objek/array JavaScript dari response.json() masuk lewat data.
+      // Menyusun komponen card artikel secara dinamis
+      const obj1 = data["standings"][0];
+      let i = 0;
+      for (let iter_1 in obj1){
+        if (i == 3){            
+          const name = obj1[iter_1][0]["team"]["name"];
+          const points = obj1[iter_1][0]["points"];
+          const playedGames = obj1[iter_1][0]["playedGames"];
+          const won = obj1[iter_1][0]["won"];
+          const draw = obj1[iter_1][0]["draw"];
+          const lost = obj1[iter_1][0]["lost"];
+        
+          const card = document.createElement("div");
+          card.className = "card blue-grey darken-1";
+          card.innerHTML = `<div class="card-content white-text">
+                              <span class="card-title">${name} - ${points} Points</span>
+                              <p>Played = ${playedGames}</p>
+                              <p>Win = ${won}</p>
+                              <p>Draw = ${draw}</p>
+                              <p>Lost = ${lost}</p>
+                            </div>
+                            <div class="card-action">
+                              <a href="#">Full LaLiga Table</a>                        
+                            </div>`;
+            
+          document.getElementById("articles").appendChild(card); 
+        }
+        i++;
+      }               
+    })
+    .catch(error);
+}
+
+// Blok kode untuk melakukan request data json
+const getLigue1Standings = () => {
+  fetch(urlLigue1Standings, {
+      headers: {
+          'X-Auth-Token' : '0e42cd9c1953405f81134678fb75b5dd'
+      }
+  })
+    .then(status)
+    .then(json)
+    .then( (data) => {
+      // Objek/array JavaScript dari response.json() masuk lewat data.
+      // Menyusun komponen card artikel secara dinamis
+      const obj1 = data["standings"][0];
+      let i = 0;
+      for (let iter_1 in obj1){
+        if (i == 3){            
+          const name = obj1[iter_1][0]["team"]["name"];
+          const points = obj1[iter_1][0]["points"];
+          const playedGames = obj1[iter_1][0]["playedGames"];
+          const won = obj1[iter_1][0]["won"];
+          const draw = obj1[iter_1][0]["draw"];
+          const lost = obj1[iter_1][0]["lost"];
+        
+          const card = document.createElement("div");
+          card.className = "card blue-grey darken-1";
+          card.innerHTML = `<div class="card-content white-text">
+                              <span class="card-title">${name} - ${points} Points</span>
+                              <p>Played = ${playedGames}</p>
+                              <p>Win = ${won}</p>
+                              <p>Draw = ${draw}</p>
+                              <p>Lost = ${lost}</p>
+                            </div>
+                            <div class="card-action">
+                              <a href="#">Full LaLiga Table</a>                        
+                            </div>`;
+            
+          document.getElementById("articles").appendChild(card); 
+        }
+        i++;
+      }               
+    })
+    .catch(error);
+}
+
+
+const getAllLeaguesStandings = async () => {
+  
+  getBundesligaStandings();
+  getEredivisieStandings();
+  getEPLStandings();
+  getLaLigaStandings();
+  getLigue1Standings();
+
+
+} 
